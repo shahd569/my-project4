599233ffaa9b7477571e3d924350604ec7870f53
shahd al haj hasan <138650757+shahd569@users.noreply.github.com>
2025-06-22 10:29:23 +0300

    Update LoanEvaluatorTests.cs

--- LoanEvaluator.Tests/LoanEvaluatorTests.cs
+++ LoanEvaluator.Tests/LoanEvaluatorTests.cs
@@ -1,19 +1,3 @@
-﻿// using Xunit;
-// using LoanApp;
-// namespace LoanApp.Tests
-// {
-//     public class LoanEvaluatorTests
-//     {
-//         [Fact]
-//         public void GetLoanEligibility_Should_Return_NotEligible_When_Income_Low()
-//         {
-//             var evaluator = new LoanEvaluator();
-//             var result = evaluator.GetLoanEligibility(1500, true, 800, 5, true);
-//             Assert.Equal("Not Eligible", result);
-//         }
-//     }
-// }
-
 using Xunit;
 using LoanApp;
 
